ab1ea0720ebab0d28ae851f000554f83f4d5917f
dmgame <denis.m.pcspace@gmail.com>
2019-02-22 13:44:44 +0200

    lesson 10 interceptors

---
+++ src/app/modules/user/components/user-profile-images/user-profile-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-images',
+  templateUrl: './user-profile-images.component.html',
+  styleUrls: ['./user-profile-images.component.css']
+})
+export class UserProfileImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
