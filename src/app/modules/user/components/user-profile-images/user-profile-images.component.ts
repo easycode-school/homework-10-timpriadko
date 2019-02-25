e8e3d215f3fc650b25de48d73fdb9a34a8c348fd
unknown <timpriadko@gmail.com>
2019-02-25 21:45:36 +0200

    homework

--- src/app/modules/user/components/user-profile-images/user-profile-images.component.ts
+++ src/app/modules/user/components/user-profile-images/user-profile-images.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { AuthGlobalService } from 'src/app/services/auth-global.service';
 
 @Component({
   selector: 'app-user-profile-images',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile-images.component.css']
 })
 export class UserProfileImagesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public userImgs: [];
+  public activeUser: string;
+  public activeTab = 'selfies';
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private userService: UserService,
+    private auth: AuthGlobalService
+  ) {
+    this.activeUser = this.auth.getUserId;
   }
 
+  ngOnInit() {
+    const id = this.activeRoute.snapshot.params['id'];
+    this.userService.getUserImages(id).subscribe((data) => {
+      this.userImgs = data.images;
+      console.log(data.images);
+    });
+  }
 }
